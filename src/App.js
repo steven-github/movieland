a8884a3008975d5ea8e7adf090a11272d7da4b5a
Steven S <freelancers2010@gmail.com>
2022-12-15 20:37:48 -0600

    Including api fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,85 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
 
-function App() {
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=912103f7";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log("title", title);
+    console.log("data", data);
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovie("hulk");
+  }, []);
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="container mx-auto">
+      <div className="flex flex-col gap-4 justify-center items-center mb-8">
+        <h1 className="text-4xl text-gray-900 dark:text-white my-8">
+          MovieLand
+        </h1>
+        <form className="flex flex-col w-full px-8">
+          <label
+            for="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only"
+          >
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+              placeholder="Movie name..."
+              required
+              onChange={() => {}}
+            />
+            <button
+              type="submit"
+              className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => {}}
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="flex flex-row gap-8 justify-start mb-8">
+        {movies?.length > 0 ? (
+          <>
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </>
+        ) : (
+          <>No movies found</>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
